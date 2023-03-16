efe9dd04257e24545c8db9d944a8ffec28c3cfa9
Pipo <pipao.pipo@gmail.com>
2023-03-16 15:32:26 -0300

    act servicios

--- src/services/usuarios.js
+++ src/services/usuarios.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 
-//const baseUrl = 'http://esme.cuquicalvano.com:3000/'
-const  baseUrl ='http://localhost:4000/'
+const baseUrl = 'http://esme.cuquicalvano.com:3000/'
+//const  baseUrl ='http://localhost:4000/'
 
 
 let token = null
