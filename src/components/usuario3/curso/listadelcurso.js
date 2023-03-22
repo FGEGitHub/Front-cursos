3115720c88747b8f77670128ce3098f2a4639bcf
Pipo <pipao.pipo@gmail.com>
2023-03-22 13:14:30 -0300

    vista

--- src/components/usuario3/curso/listadelcurso.js
+++ src/components/usuario3/curso/listadelcurso.js
@@ -19,7 +19,7 @@ import TableBody from '@mui/material/TableBody';
 import Button from "@mui/material/Button";
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
-
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -47,8 +47,9 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 const TablaNotificaciones = (props) => {
     const [clases, setClases] = useState([''])
     const [usuario, setUsuario] = useState([''])
+
+    const [vista, setvista] = useState(true)
     const navigate = useNavigate();
-    
   let params = useParams()
   let id = params.id
     useEffect(() => {
@@ -57,7 +58,11 @@ const TablaNotificaciones = (props) => {
 
 
     }, [])
+    const cambiarvista =  () => {
+        setvista(!vista)
 
+
+    }
 
     const traer = async () => {
         try {
@@ -177,7 +182,8 @@ const TablaNotificaciones = (props) => {
         <div>
             {clases ? <>
                 <div>
-                  
+                <Button variant="contained" onClick={cambiarvista} >Vista<RemoveRedEyeIcon/></Button>
+                {vista ? <>
                 <TableContainer component={Paper}>
       <Table sx={{ minWidth: "20%",maxWidth: "1000%"}} aria-label="customized table">
         <TableHead>
@@ -227,6 +233,7 @@ const TablaNotificaciones = (props) => {
         </TableBody>
       </Table>
     </TableContainer>
+    </>:<> 
                         <>
                             <MUIDataTable
 
@@ -247,7 +254,7 @@ const TablaNotificaciones = (props) => {
                         </>
                         /* 
                                       */
-
+                        </> }
                  
                 </div>
             </> : <></>}
