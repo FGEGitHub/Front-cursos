f57bfb00c8fdf4e39bf597b640a9ed81b33cf72e
FGEGitHub <pipao.pipo@gmail.com>
2023-05-29 00:28:31 -0300

    info pers

--- src/components/fiscalizacion/admin/persona/info.js
+++ src/components/fiscalizacion/admin/persona/info.js
@@ -51,260 +51,238 @@ const FichaAxios = (props) => {
     
    {cliente.map((client) =>( 
     <div className="profile">
-      <Grid Container style={{ direction: "column", alignItems:"center", justifyContent: "center", display: "flex"}}>
-        <Grid item xs={8} style={{ direction: "column", justifyContent: "center", display: "flex" }}>
-        <Avatar sx={{ width: 170, height: 140 }}> <AccountCircle fontSize="large"/> </Avatar>
+      <Grid container style={{ flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+  <Grid item xs={12} style={{ display: "flex", justifyContent: "center" }}>
+    <Avatar sx={{ width: 170, height: 140 }}>
+      <AccountCircle fontSize="large" />
+    </Avatar>
+  </Grid>
+  <Grid item xs={12}>
+    <Container>
+      <Box>
+        <h5>Datos Personales del Cliente</h5>
+        <Modificar
+          id={client.id}
+          dni={client.dni}
+          nombre={client.nombre}
+          apellido={client.apellido}
+          vegano={client.vegano}
+          movilidad={client.movilidad}
+          celiaco={client.celiaco}
+          telefono={client.telefono}
+          telefono2={client.telefono2}
+          domicilio={client.domicilio}
+          traer={async () => {
+            const cliente = await servicioFidei.traerdatosdepersona(id);
+            setCliente(cliente);
+          }}
+        />
+      </Box>
+      <Box>
+        <TextField
+          label="DNI"
+          id="dni"
+          value={client.dni}
+          name="dni"
+          variant="filled"
+          sx={{ margin: "10px" }}
+          InputProps={{
+            readOnly: false,
+            startAdornment: (
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            ),
+          }}
+        />
+
+        <TextField
+          label="Nombre"
+          id="Nombre"
+          value={client.nombre}
+          name="nombre"
+          variant="filled"
+          sx={{ margin: "10px" }}
+          InputProps={{
+            readOnly: false,
+            startAdornment: (
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Box>
+
+      <Box>
+        <TextField
+          label="Apellido"
+          id="email"
+          value={client.apellido}
+          name="nombre"
+          variant="filled"
+          sx={{ margin: "10px" }}
+          InputProps={{
+            readOnly: false,
+            startAdornment: (
+              <InputAdornment position="start">
+                <EmailIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+
+        <TextField
+          label="Domicilio"
+          id="Localidad"
+          value={client.domicilio}
+          variant="filled"
+          sx={{ margin: "10px" }}
+          InputProps={{
+            readOnly: false,
+            startAdornment: (
+              <InputAdornment position="start">
+                <HomeWorkIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Box>
+
+      <Box>
+        <TextField
+          label="Numero de Telefono"
+          id="numero de telefono"
+          value={client.telefono}
+          name="telefono"
+          variant="filled"
+          sx={{ margin: "10px" }}
+          InputProps={{
+            readOnly: false,
+            startAdornment: (
+              <InputAdornment position="start">
+                <LocalPhoneIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+
+        <TextField
+          label="Numero de Telefono"
+          id="numero de telefono"
+          value={client.telefono2}
+          name="telefono2"
+          variant="filled"
+          sx={{ margin: "10px" }}
+          InputProps={{
+            readOnly: false,
+            startAdornment: (
+              <InputAdornment position="start">
+                <LocalPhoneIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Box>
+
+      <Box>
+        <TextField
+          label="Vegano"
+          id="Razon"
+          defaultValue=""
+          value={client.vegano}
+          variant="filled"
+          sx={{ margin: "10px" }}
+          InputProps={{
+            readOnly: false,
+            startAdornment: (
+              <InputAdornment position="start">
+                <LocalPhoneIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+
+        <TextField
+          label="Movilidad"
+          id="dirección"
+          defaultValue=""
+          value={client.movilidad}
+          variant="filled"
+          sx={{ margin: "10px" }}
+          InputProps={{
+            readOnly: false,
+            startAdornment: (
+              <InputAdornment position="start">
+                <HomeIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Box>
+
+      <Box>
+        <TextField
+          label="Celiaco"
+          id="Razon"
+          defaultValue=""
+          value={client.celiaco}
+          variant="filled"
+          sx={{ margin: "10px" }}
+          InputProps={{
+            readOnly: false,
+            startAdornment: (
+              <InputAdornment position="start">
+                <LocalPhoneIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+
+        <TextField
+          label="Donde vota"
+          id="dirección"
+          defaultValue=""
+          value={client.nombreescuela}
+          variant="filled"
+          sx={{ margin: "10px" }}
+          InputProps={{
+            readOnly: false,
+            startAdornment: (
+              <InputAdornment position="start">
+                <HomeIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Box>
+
+      <Box>
+        <Grid container justifyContent="center">
+          {editMode ? (
+            <Grid item xs={12} md={8}>
+              <div className="profile-form-button">
+                <Button variant="outlined" sx={{ marginRight: "10px" }} onClick={() => setEditMode(false)}>
+                  Cancelar
+                </Button>
+                <Button variant="contained">Enviar</Button>
+              </div>
+            </Grid>
+          ) : (
+            <Grid item xs={12}>
+              <div className="profile-edit-button"></div>
+            </Grid>
+          )}
         </Grid>
-        <Grid item xs={8}style={{ }}>
-  
-            <Container>
-            <Box>
-            <h5>
-            Datos Personales del Cliente
-            </h5>
-            <Modificar
-              id = {client.id}
-            dni = {client.dni}
-            nombre = {client.nombre}
-            apellido = {client.apellido}
-            vegano = {client.vegano}
-            movilidad = {client.movilidad}
-            celiaco = {client.celiaco}
-            telefono = {client.telefono}
-            telefono2= {client.telefono2}
-            domicilio= {client.domicilio}
-            traer ={async() => {
-       
-   
-              const  cliente = await servicioFidei.traerdatosdepersona(id)
-              
-              setCliente(cliente)
-          
-             
-          
-              ;
-            }  }
+      </Box>
+    </Container>
+  </Grid>
 
-            />
-            </Box>
-       
-              <Box>
-              <TextField
-                  label="DNI"
-                  id="dni"
-                 // defaultValue="CUIL"
-                 value= {client.dni}
-                  name= "dni"
-                  variant="filled"
-                  sx={{ margin: "10px" }}
-                  InputProps={{
-                    readOnly: false,
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <AccountCircle />
-                      </InputAdornment>
-                    )
-                  }}
-                />
+  <Grid item xs={12} style={{ display: "flex", justifyContent: "center" }}>
+   contenido adicional si es necesario
+  </Grid>
+</Grid>
 
-                <TextField
-                  label="Nombre"
-                  id="Nombre"
-                  value={client.nombre}
-                  name= "nombre"
-                  variant="filled"
-                  sx={{ margin: "10px" }}
-                  InputProps={{
-                    readOnly: false,
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <AccountCircle />
-                      </InputAdornment>
-                    )
-                  }}
-                />
-              </Box>
-                   
-
-                    
-              <Box>
-             
-                <TextField
-                  label="Apellido"
-                  id="email"
-                  value={client.apellido}
-                  name= "nombre"
-                  variant="filled"
-                  sx={{ margin: "10px" }}
-                  InputProps={{
-                    readOnly: false,
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <EmailIcon />
-                      </InputAdornment>
-                    )
-                  }}
-                />
-
-                <TextField
-                  label="Domicilio"
-                  id="Localidad"
-                  value={client.domicilio}
-                  variant="filled"
-                  sx={{ margin: "10px" }}
-                  InputProps={{
-                    readOnly: false,
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <HomeWorkIcon />
-                      </InputAdornment>
-                    )
-                  }}
-                />
-              </Box>
-              <Box>
-                <TextField
-                  label="Numero de Telefono"
-                  id="numero de telefono"
-                
-                  value={client.telefono}
-                  name="telefono"
-                  variant="filled"
-                  sx={{ margin: "10px" }}
-                  InputProps={{
-                    readOnly: false,
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <LocalPhoneIcon />
-                      </InputAdornment>
-                    )
-                  }}
-                />
-
-<TextField
-                  label="Numero de Telefono"
-                  id="numero de telefono"
-                 
-                  value={client.telefono2}
-                  name="telefono2"
-                  variant="filled"
-                  sx={{ margin: "10px" }}
-                  InputProps={{
-                    readOnly: false,
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <LocalPhoneIcon />
-                      </InputAdornment>
-                    )
-                  }}
-                />
-                  
-             
-              </Box>
-              <Box>
-                <TextField
-                  label="Vegano"
-                  id="Razon"
-                  defaultValue=""
-                  value={client.vegano}
-                  variant="filled"
-                  sx={{ margin: "10px" }}
-                  InputProps={{
-                    readOnly:false,
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <LocalPhoneIcon />
-                      </InputAdornment>
-                    )
-                  }}
-                />
-
-                <TextField
-                  label="Movilidad"
-                  id="dirección"
-                  defaultValue=""
-                  value={client.movilidad}
-                  variant="filled"
-                  sx={{ margin: "10px" }}
-                  InputProps={{
-                    readOnly: false,
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <HomeIcon />
-                      </InputAdornment>
-                    )
-                  }}
-                >
-                  
-                </TextField>
-              </Box>
-              <Box>
-                <TextField
-                  label="Celiaco"
-                  id="Razon"
-                  defaultValue=""
-                  value={client.celiaco}
-                  variant="filled"
-                  sx={{ margin: "10px" }}
-                  InputProps={{
-                    readOnly:false,
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <LocalPhoneIcon />
-                      </InputAdornment>
-                    )
-                  }}
-                />
-
-                <TextField
-                  label="Donde vota"
-                  id="dirección"
-                  defaultValue=""
-                  value={client.nombreescuela}
-                  variant="filled"
-                  sx={{ margin: "10px" }}
-                  InputProps={{
-                    readOnly: false,
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <HomeIcon />
-                      </InputAdornment>
-                    )
-                  }}
-                >
-                  
-                </TextField>
-              </Box>
-
-              <Box>
-                <columns lg={8}>
-                  {editMode ? (
-                    <div className="profile-form-button">
-                      <Button
-                        variant="outlined"
-                        sx={{ marginRight: "10px" }}
-                        onClick={() => setEditMode(false)}
-                      >
-                        Cancelar
-                      </Button>
-                      <Button variant="contained">Enviar</Button>
-                    </div>
-                  ) : (
-                    <div className="profile-edit-button">
-                    
-                    </div>
-                  )}
-                </columns>
-              </Box>
-            </Container>
-          
-        </Grid>
-
-        <Grid item xs={8} style={{ justifyContent: "center", display: "flex" }}>
-         
-    
-        </Grid>
-      </Grid>
     </div>
     ))} </>);
 }
