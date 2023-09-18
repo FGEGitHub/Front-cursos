60bdd7649fffecd91125ba8260564b332effa7da
FGEGitHub <pipao.pipo@gmail.com>
2023-09-18 09:56:22 -0300

    formulario

--- src/components/usuario6/listatodos/lista.js
+++ src/components/usuario6/listatodos/lista.js
@@ -101,7 +101,10 @@ export default function Ingresos() {
             }
          
           }}/>
-          </>:<><Desasignar/></>}
+          </>:<>
+          
+          
+          <Desasignar/></>}
           
           {/* 
  <CambiarEst
