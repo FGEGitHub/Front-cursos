69e24be1758dc7d2d608840ef7a055a07c4b7a14
FGEGitHub <pipao.pipo@gmail.com>
2023-10-18 12:49:42 -0300

    justificar

--- src/components/usuario4/asistencia/lista.js
+++ src/components/usuario4/asistencia/lista.js
@@ -32,7 +32,7 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   '&:nth-of-type(odd)': {
-    backgroundColor: "#35baf6",
+    backgroundColor: "#00e676",
   },
   // hide last border
   '&:last-child td, &:last-child th': {
