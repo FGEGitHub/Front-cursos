fca6b2ad51a0982685a9a760a24f3b8a70b92156
FGEGitHub <pipao.pipo@gmail.com>
2024-04-18 10:49:24 -0300

    grafica

--- src/components/dtc/usuario1/menu/estadistivcas.js
+++ src/components/dtc/usuario1/menu/estadistivcas.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import servicioDtc from '../../../../services/dtc'
-
+import Grafico from './grafica'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
@@ -9,8 +9,21 @@ import IconButton from '@mui/material/IconButton';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import  { useEffect, useState } from "react";
 import Widget from '../../../fiscalizacion/Widget/Widget'
+const nombresDiasSemana = [
+  'domingo',
+  'lunes',
+  'martes',
+  'miércoles',
+  'jueves',
+  'viernes',
+  'sábado'
+];
 const CardInformacionDia = (props) => {
     const [datos, setDatos] = useState()
+    const fechaActual = new Date();
+
+    // Obtener el día de la semana (0 = domingo, 6 = sábado)
+    const diaSemana = fechaActual.getDay();
   // Puedes personalizar este avatar o icono según tus necesidades
   useEffect(() => {
     traer()
@@ -34,15 +47,15 @@ const traer = async () => {
           const formattedDate = `${today.getDate()}-${today.getMonth() + 1}-${today.getFullYear()}`;
 
         //  setCurrentDate(formattedDate);
-          const historial = await servicioDtc.traerestadisticas(formattedDate)
+          const historial = await servicioDtc.traerestadisticas({fecha:formattedDate})
 setDatos(historial)   
-
+console.log(historial[0].semana)
   } catch (error) {
 
   }
 
 }
-  return (
+  return (    <>
     <Card sx={{
       cursor: 'pointer',
       background: '#eceff1',
@@ -57,26 +70,43 @@ setDatos(historial)
        {datos ? <>
              <div className="home">
                 
-                <Widget  type="Cantidad de presentes del mes: "
+                <Widget  type="Asistencias tomadas en el mes: "
                       cantidad={datos[0].presentes_totales}
                     />
                           <Widget  type=" Cantidad de usuarios que concurrieron en el mes:"
                       cantidad={datos[0].presentes_totales_reales}
                     />
-                          <Widget  type="Cantidad presentes mes pasado"
+                                 <Widget  type="antidad de usuarios que concurrieron en el mes pasado:"
                       cantidad={datos[0].presentes_totales_reales_mespasado}
                     />
-                          <Widget  type="presentes en la semana"
+                          
+             
+                   
+                          
+                    </div>
+                    <div className="home">
+                    <Widget  type="Asistencias en la semana"
                       cantidad={datos[0].presentes_totales_semana}
+                    /> {nombresDiasSemana[diaSemana]}
+                                <Widget  type={"presentes semana pasada hasta el "+nombresDiasSemana[diaSemana]}
+                      cantidad={datos[0].pres_Semanapasada}
                     />
-                           <Widget  type="presentes reales en lasemana"
+                           <Widget  type="Usuarios en la semana"
                       cantidad={datos[0].presentes_totales_reales_semana}
                     />
-                          
+                              <Widget  type={"Usuarios en la semana pasada hasta el  "+nombresDiasSemana[diaSemana]}
+                      cantidad={datos[0].pres_Semanal_real_semanapasada}
+                    />
+
                     </div>
                     </>:<></>}
       </CardContent>
     </Card>
+    {datos? <>
+    <Grafico
+    semana={datos[0].semana}
+    semanapasada={datos[0].semanapasada}/></>:<></>}
+</>
   );
 };
 
