7454a9e667e8889d9fe8de8ddc0b2244fa419131
FGEGitHub <pipao.pipo@gmail.com>
2024-09-24 14:43:35 -0300

    l

--- src/pages/inscripciones/index.js
+++ src/pages/inscripciones/index.js
@@ -1,7 +1,7 @@
 
 
 import { useNavigate, useParams } from "react-router-dom";
-import Formulario from '../../components/componenteinscripcion/formularioprueba'
+import Formulario from '../../components/componenteinscripcion/cerrado'
 import React, { useEffect, useState } from "react";
 import {
     Button,
