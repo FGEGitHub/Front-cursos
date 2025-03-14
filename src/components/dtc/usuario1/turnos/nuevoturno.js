8773f868b9153ffc422c29d4858b831c7098bc2f
FGEGitHub <pipao.pipo@gmail.com>
2025-03-14 17:34:17 -0300

    nuevoturno hoarios selec

--- src/components/dtc/usuario1/turnos/nuevoturno.js
+++ src/components/dtc/usuario1/turnos/nuevoturno.js
@@ -1,66 +1,53 @@
-import Box from '@mui/material/Box';
+import Box from '@mui/material/Box'; 
 import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
-import { Button,FormControlLabel, FormControl, Radio, RadioGroup,Typography } from '@mui/material';
+import Select from '@mui/material/Select';
+import { Button, FormControlLabel, FormControl, Radio, RadioGroup, Typography, InputLabel } from '@mui/material';
 import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
-import servicioDtc from '../../../../services/dtc'
-import NativeSelect from '@mui/material/NativeSelect';
+import DialogActions from '@mui/material/DialogActions';
+import servicioDtc from '../../../../services/dtc';
 import Tooltip from '@material-ui/core/Tooltip';
 import React, { useEffect, useState, Fragment } from "react";
-import DialogActions from '@mui/material/DialogActions';
-import InputLabel from '@mui/material/InputLabel';
 import styled from 'styled-components';
-
 
 const StyledParagraph = styled.p`
   font-family: 'Montserrat', sans-serif;
 `;
 
+
+
+const horariosDisponibles = [
+"08:20", "09:00",  "10:20", "11:00", "11:40", "14:00", "14:40", "15:20", "16:00", "16:40", "17:20"
+];
+
 export default function SelectTextFields(props) {
   const [open, setOpen] = React.useState(false);
-  //const usuario  = useUser().userContext
-  const [form, setForm] = useState({
-    fecha:props.fecha
-  })
-  const [usuario, setUsuario] = useState()
-  const [profesionales, setProfesionales] = useState(false)
-
-
-
-
-
-
+  const [form, setForm] = useState({ fecha: props.fecha, horario: "" });
+  const [usuario, setUsuario] = useState();
+  const [profesionales, setProfesionales] = useState(false);
 
   const handleChange = (e) => {
-    console.log(form)
-    setForm({ ...form, [e.target.name]: e.target.value })
-  }
-  const traerprof = async (event) => {
-    const loggedUserJSON = window.localStorage.getItem('loggedNoteAppUser')
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
 
-    const user = JSON.parse(loggedUserJSON)
-    setUsuario(user)
-    if (user.nivel == 40){
-      const nov = await servicioDtc.traerprofesionales()
-      console.log(nov)
-      setProfesionales(nov[0])
+  const traerprof = async () => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteAppUser');
+    const user = JSON.parse(loggedUserJSON);
+    setUsuario(user);
+    if (user.nivel === 40) {
+      const nov = await servicioDtc.traerprofesionales();
+      setProfesionales(nov[0]);
+    } else if (user.nivel === 20 || user.nivel === 23) {
+      const nov = await servicioDtc.traerpsicologos();
+      setProfesionales(nov[0]);
     }
-    if (user.nivel == 20 || user.nivel == 23){
-      const nov = await servicioDtc.traerpsicologos() 
-      console.log(nov)
-      setProfesionales(nov[0])
-    }
-
- 
   };
 
   const handleClickOpen = () => {
-setForm({fecha:props.fecha})
+    setForm({ fecha: props.fecha, horario: "" });
     setOpen(true);
-    traerprof()
-
-
+    traerprof();
   };
 
   const handleClose = () => {
@@ -68,130 +55,76 @@ setForm({fecha:props.fecha})
   };
 
   const handleDeterminar = async (event) => {
-
     try {
       event.preventDefault();
-      const loggedUserJSON = window.localStorage.getItem('loggedNoteAppUser')
+      const loggedUserJSON = window.localStorage.getItem('loggedNoteAppUser');
+      const user = JSON.parse(loggedUserJSON);
+      setUsuario(user);
+      const mergedJSON = { ...form, id_psic: user.id };
 
-      const user = JSON.parse(loggedUserJSON)
-      setUsuario(user)
-      const mergedJSON = {
-        ...form,
-        ...{id_psic:user.id,
-       }
-      };
-      if(user.nivel==41 || user.nivel==40 ){
-        const nov = await servicioDtc.agregarturnocadia(mergedJSON)
-        alert(nov)
-      }else{
-      const nov = await servicioDtc.agregarturno(mergedJSON)
-      alert(nov)}
+      if (user.nivel === 41 || user.nivel === 40) {
+        const nov = await servicioDtc.agregarturnocadia(mergedJSON);
+        alert(nov);
+      } else {
+        const nov = await servicioDtc.agregarturno(mergedJSON);
+        alert(nov);
+      }
     } catch (error) {
       console.error(error);
-      console.log('Error algo sucedio')
-      console.log(form.fecha)
-props.traer(form.fecha)
-
+      props.traer(form.fecha);
     }
-  
-    props.traer()
-
+    props.traer();
     setOpen(false);
   };
 
-  const [currency, setCurrency] = React.useState('EUR');
-
-  /*   const handleChange = (event) => {
-      setCurrency(event.target.value);
-    }; */
-
-
   return (
-
-
-
-
-    <Box
-
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      < Tooltip title="Nuevo turno">
-        <Button variant="contained" onClick={handleClickOpen}> Nuevo  </Button>
-
+    <Box sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }} noValidate autoComplete="off">
+      <Tooltip title="Nuevo turno">
+        <Button variant="contained" onClick={handleClickOpen}> Nuevo </Button>
       </Tooltip>
       <Dialog open={open} onClose={handleClose}>
         <DialogContent>
-
-        
-            <h3>
-              <b> NUEVO TURNO</b></h3>
-              <InputLabel variant="outlined" htmlFor="uncontrolled-native">
-
-              <Typography variant="h5" component="div" color="black">
-                                            <StyledParagraph>
-                                          horario
-                                            </StyledParagraph>
-                                        </Typography>
-                                    </InputLabel>
-              <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              label="horario"
-              name="horario"
-              onChange={handleChange}
-              fullWidth
-              variant="standard"
-            />
-
-{usuario ? <>
-
-  {usuario.nivel==40 || usuario.nivel==20 || usuario.nivel==23  ? <>
-<br/>
-    {profesionales ? <>
-      <InputLabel variant="outlined" htmlFor="uncontrolled-native">
-                                        <Typography variant="h5" component="div" color="black">
-                                            <StyledParagraph>
-                                             Profesional
-                                            </StyledParagraph>
-                                        </Typography>
-                                    </InputLabel>
-                                            <FormControl>
-                                                <RadioGroup name="profesional" onChange={handleChange}>
-
-
-
-                                                    {profesionales.map((row) => (
-                                                        <FormControlLabel value={row.id} control={<Radio />} label={row.nombre} />
-
-                                                    ))}
-                                                </RadioGroup>
-                                            </FormControl> </> : <>Cargando</>}
-  </>:<>
-  
-  </>}
-
-
-</>:<></>}
-
-            <DialogActions>
-
-
-              <>
-              {form.horario   ? <> <Button variant="contained" color="primary" onClick={handleDeterminar}> crear </Button></> :  <>Completar los datos</>}
-             </>
-              <Button variant="outlined" color="error" style={{ marginLeft: "auto" }} onClick={handleClose}>Cancelar</Button>
-            </DialogActions>
-
-
+          <h3><b>NUEVO TURNO</b></h3>
+          <InputLabel>Horario</InputLabel>
+          <Select
+            name="horario"
+            value={form.horario}
+            onChange={handleChange}
+            fullWidth
+          >
+            {horariosDisponibles.map((horario) => (
+              <MenuItem key={horario} value={horario}>{horario}</MenuItem>
+            ))}
+          </Select>
+          {usuario && (usuario.nivel === 40 || usuario.nivel === 20 || usuario.nivel === 23) && (
+            <>
+              <br />
+              {profesionales ? (
+                <>
+                  <InputLabel>Profesional</InputLabel>
+                  <FormControl>
+                    <RadioGroup name="profesional" onChange={handleChange}>
+                      {profesionales.map((row) => (
+                        <FormControlLabel key={row.id} value={row.id} control={<Radio />} label={row.nombre} />
+                      ))}
+                    </RadioGroup>
+                  </FormControl>
+                </>
+              ) : (
+                <>Cargando...</>
+              )}
+            </>
+          )}
+          <DialogActions>
+            {form.horario ? (
+              <Button variant="contained" color="primary" onClick={handleDeterminar}>Crear</Button>
+            ) : (
+              <>Completar los datos</>
+            )}
+            <Button variant="outlined" color="error" style={{ marginLeft: "auto" }} onClick={handleClose}>Cancelar</Button>
+          </DialogActions>
         </DialogContent>
       </Dialog>
-    </Box >
-
-
+    </Box>
   );
 }
